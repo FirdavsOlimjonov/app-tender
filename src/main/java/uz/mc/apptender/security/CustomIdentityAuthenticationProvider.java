b9bc8f75dd81382bd554ad07aa40726c5e23a351
Firdavs_Olimjonov <firdavsolimjonov25@gmail.com>
2023-02-09 17:19:29 +0500

    2

--- src/main/java/uz/mc/apptender/security/CustomIdentityAuthenticationProvider.java
+++ src/main/java/uz/mc/apptender/security/CustomIdentityAuthenticationProvider.java
@@ -16,6 +16,9 @@ import uz.mc.apptender.modules.enums.RoleEnum;
 import uz.mc.apptender.repositories.UserRepository;
 import uz.mc.apptender.utils.MessageConstants;
 
+import java.util.Objects;
+import java.util.Optional;
+
 @Component
 @RequiredArgsConstructor
 public class CustomIdentityAuthenticationProvider implements AuthenticationProvider {
@@ -33,41 +36,31 @@ public class CustomIdentityAuthenticationProvider implements AuthenticationProvi
     // and validate the user
     // we are hardcoding for a single user for demo purposes
     UserDetails isValidUser(String username, String password, uz.mc.apptender.modules.User user) {
-        if (passwordEncoder.matches(password, user.getPassword())) {
-                if (username.equalsIgnoreCase(adminUsername) && password.equalsIgnoreCase(adminPassword))
-                    return User
-                            .withUsername(username)
-                            .password(password)
-                            .roles(RoleEnum.ADMIN.name())
-                            .build();
-                return User
-                        .withUsername(username)
-                        .password(password)
-                        .roles(RoleEnum.USER.name())
-                        .build();
-        }
-        throw RestException.restThrow(MessageConstants.FULL_AUTHENTICATION_REQUIRED, HttpStatus.UNAUTHORIZED);
+        if (passwordEncoder.matches(password, user.getPassword()) && Objects.equals(user.getUsername(), username))
+            return user;
+        return null;
     }
 
     @Override
     public Authentication authenticate(Authentication authentication) {
         String username = authentication.getName();
         String password = authentication.getCredentials().toString();
-        uz.mc.apptender.modules.User user = userRepository.findByUsernameEqualsIgnoreCase(username).orElseThrow(
-                () -> RestException.restThrow(MessageConstants.USER_NOT_FOUND, HttpStatus.FORBIDDEN));
+        Optional<uz.mc.apptender.modules.User> optionalUser = userRepository.findByUsernameEqualsIgnoreCase(username);
+        if (optionalUser.isEmpty())
+            throw new BadCredentialsException("User not found");
 
+        uz.mc.apptender.modules.User user = optionalUser.get();
         UserDetails userDetails = isValidUser(username, password, user);
-        if (userDetails != null) {
-            return new UsernamePasswordAuthenticationToken(
-                    user, user.getAuthorities(), null);
-        } else {
+
+        if (userDetails == null)
             throw new BadCredentialsException("Incorrect user credentials !!");
-        }
+
+        return new UsernamePasswordAuthenticationToken(
+                user, user.getAuthorities(), null);
     }
 
     @Override
     public boolean supports(Class<?> authenticationType) {
-        return authenticationType
-                .equals(UsernamePasswordAuthenticationToken.class);
+        return authenticationType.equals(UsernamePasswordAuthenticationToken.class);
     }
 }
